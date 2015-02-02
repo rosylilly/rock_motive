b4fc785c2e6030cdcc7ae1dab66a701182721894
Sho Kusano <rosylilly@aduca.org>
2015-02-03 06:00:19 +0900

    Fix require

--- lib/rock_motive.rb
+++ lib/rock_motive.rb
@@ -1,2 +1,3 @@
 require 'rock_motive/version'
+require 'rock_motive/interaction'
 require 'rock_motive/railtie' if defined?(::Rails)
--- lib/rock_motive/railtie.rb
+++ lib/rock_motive/railtie.rb
@@ -5,8 +5,4 @@ class RockMotive::Railtie < ::Rails::Railtie
     app.paths.add('app/interactions', eager_load: true)
     app.paths.add('app/roles', eager_load: true)
   end
-
-  initializer 'rock_motive.interaction' do
-    require 'rock_motive/interaction'
-  end
 end
